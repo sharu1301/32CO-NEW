78c673afa38ef9989d6ce9b03d7ca86637cdf19c
root <root@ip-172-31-92-182.ec2.internal>
2025-07-18 20:55:20 +0000

    Fix RDS family parameter error

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,3 +1,22 @@
+resource "aws_db_parameter_group" "nodejs_db" {
+  name   = "nodejs-app-db-pg"
+  family = "postgres14"
+  
+  parameter {
+    name  = "autovacuum"
+    value = "1"
+  }
+  
+  parameter {
+    name  = "client_encoding"
+    value = "utf8"
+  }
+  
+  tags = {
+    Name = "nodejs-app-db-pg"
+  }
+}
+
 module "rds" {
   source  = "terraform-aws-modules/rds/aws"
   version = "~> 5.0"
@@ -6,7 +25,7 @@ module "rds" {
 
   engine               = "postgres"
   engine_version       = "14.5"
-  family               = "postgres14"   # ADD THIS LINE
+  parameter_group_name = aws_db_parameter_group.nodejs_db.name  # REFERENCE CUSTOM PG
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
 
@@ -23,9 +42,13 @@ module "rds" {
   maintenance_window = "Mon:00:00-Mon:03:00"
   backup_window      = "03:00-06:00"
   backup_retention_period = 7
+  skip_final_snapshot     = true  # For assessment only - remove in production
 
   # Monitoring
   performance_insights_enabled = true
+
+  # Option group settings
+  major_engine_version = "14"
 
   tags = {
     Name = "nodejs-app-database"
