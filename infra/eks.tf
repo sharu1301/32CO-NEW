5e6e6fa6e20c998d57709a003eec24297e90e56d
root <root@ip-172-31-92-182.ec2.internal>
2025-07-18 23:20:40 +0000

    FIX: eks ossue

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,34 +1,35 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 20.0" 
 
   cluster_name    = var.cluster_name
   cluster_version = "1.27"
+  
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # Endpoint access settings
-  cluster_endpoint_private_access      = true
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
-
+  # IAM Role for Service Accounts (IRSA)
   enable_irsa = true
 
+  # Node Group Configuration
   eks_managed_node_groups = {
     nodejs = {
-      min_size       = 2
-      max_size       = 5
-      desired_size   = 2
+      min_size     = 2
+      max_size     = 5
+      desired_size = 2
       instance_types = ["t3.medium"]
       capacity_type  = "SPOT"
     }
   }
 
   cluster_addons = {
-    coredns   = { resolve_conflicts = "OVERWRITE" }
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
     kube-proxy = {}
-    vpc-cni   = { resolve_conflicts = "OVERWRITE" }
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
   }
 
   tags = {
@@ -36,3 +37,11 @@ module "eks" {
   }
 }
 
+resource "aws_ecr_repository" "nodejs_app" {
+  name                 = "nodejs-app"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
