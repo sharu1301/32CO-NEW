52e1f28f8296c461ed7f09b7a4be6cb9b7efa277
root <root@ip-172-31-92-182.ec2.internal>
2025-07-19 14:54:14 +0000

    Add root (/) route to Node.js app

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => res.send('Welcome to the Node.js app!'));
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.get('/ready', (req, res) => res.json({ status: 'ready' }));
 
